80bb222886268443865f9f3054d5c8e5219e39e7
Capolaretti <danielec1998@gmail.com>
2022-08-11 11:26:43 +0200

    first_task_completion

--- first_task.py
+++ first_task.py
@@ -1,10 +1,12 @@
 import cv2
+from cv2 import pointPolygonTest
 import numpy as np
 from matplotlib import pyplot as plt
 
 
-image=cv2.imread("./asset/first_task/C0_000002.png",cv2.IMREAD_GRAYSCALE)
-img = cv2.imread("./asset/first_task/C0_000002.png")
+image=cv2.imread("./asset/first_task/C0_000001.png",cv2.IMREAD_GRAYSCALE)
+img = cv2.imread("./asset/first_task/C1_000001.png")
+final_img=img.copy()
 
 t_image=cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,11,2)
 #,t_image=cv2.threshold(image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
@@ -48,6 +50,9 @@ contours_edge,hier_edge=cv2.findContours(dilated,cv2.RETR_TREE,cv2.CHAIN_APPROX_
 contours_edge.sort(key=cv2.contourArea,reverse=True)
 cv2.drawContours(img,contours_edge,-1,(0,0,255),1)
 
+#print(cv2.contourArea(contours_edge[0]))
+#cv2.waitKey(0)
+
 cv2.imshow('Canny Edge Detection', img)
 cv2.waitKey(0)
 
@@ -63,12 +68,32 @@ for idx, contour in enumerate(contours_edge):
 
     locs = np.where(blank == 255)
     pixels = dilated[locs]
-    if (np.mean(pixels) < 240):
+
+    # First approach to eliminate small contours: ignore all contours smaller than the smallest defect possible (in this case the small hole in the first image)
+    if (np.mean(pixels) < 240 and (cv2.contourArea(contour)>=126.0) and (cv2.contourArea(contour)<50000.0)):
         filtered_contours.append(contour)
 
-for i, c in enumerate(filtered_contours):
-    blank = np.zeros(image.shape, dtype='uint8')
-    cv2.drawContours(blank,filtered_contours,i,(255,255,255),-1)
 
-    cv2.imshow('Single Contour Mask', blank)
-    cv2.waitKey(0)
+inside=np.zeros(len(filtered_contours), np.uint8)
+for i, c in enumerate(filtered_contours):
+    for j, k in enumerate(filtered_contours):
+        if (i!=j):
+            #print(type(int(c[0][0][0])))
+            #cv2.waitKey(0)
+            if(pointPolygonTest(k,(int(c[0][0][0]),int(c[0][0][1])), False)>=0):
+                inside[i]=1
+                
+print(inside)
+cv2.waitKey(0)
+
+defects=[]
+for i,v in enumerate(inside):
+    if(v==0):
+        defects.append(filtered_contours[i])
+
+
+
+cv2.drawContours(final_img,defects,-1,(0,0,255),2)
+
+cv2.imshow('Single Contour Mask', final_img)
+cv2.waitKey(0)
